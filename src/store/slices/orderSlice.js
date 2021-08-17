a3a91de42565beec5cd9365d1ddeb2f7d125d337
HarshDhoble <harshdhoble26@gmail.com>
2021-08-17 17:40:49 +0530

    added orderSlice

---
+++ src/store/slices/orderSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+const OrderSlice = createSlice({
+    name : "order",
+    initialState:{
+        //localStorage.getitem('orderItems') | orderItems : [],
+    },
+    reducers : {
+        addOrderItem: (state, action) => {
+            state.orderItems = [...order.cartItems, action.payload];
+            localStorage.setItem("orderItems", JSON.stringify(state.orderItems));
+        },
+    },
+});
+export const { addOrderItems } = OrderSlice.actions;
+export default OrderSlice.reducer;
+
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
-
+import orderReducer from "./slices/orderSlice";
 export default configureStore({
   reducer: {
     cart: cartReducer,
+    order : orderReducer,
   },
 });
