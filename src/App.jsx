cedeb0713663c49ec85dd5428f0860a58696b2b1
seebham <shubhamisdev@gmail.com>
2021-08-17 15:07:53 +0530

    initial v0.0.1 💥

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h2" color="textPrimary">
+        The Only Store
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
