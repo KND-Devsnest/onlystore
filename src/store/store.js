612fdf0d8d710a878e3a844e7856017d62efafd9
aguyran <59661860+aguyran@users.noreply.github.com>
2021-08-17 17:31:30 +0530

    basic productSlice added

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
+import productsSlice from "./slices/productsSlice";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
+    products: productsSlice,
   },
 });
