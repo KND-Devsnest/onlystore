d55baa8d613c284e0d42769c66ade8132ae0bf20
Swanand Buva <swanandev@gmail.com>
2021-08-17 18:42:31 +0530

    Added WishlistSlice

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -3,21 +3,37 @@ import { createSlice } from "@reduxjs/toolkit";
 const CartSlice = createSlice({
   name: "cart",
   initialState: {
-    cartItems: [],
+    cartItems: localStorage.getItem("cart") | [],
+    totalPrice: localStorage.getItem("cart_totalPrice") | 0,
   },
   reducers: {
+    //adds item to the cart
     addCartItem: (state, action) => {
       state.cartItems = [...state.cartItems, action.payload];
+      state.totalPrice += 0; //change after data source decided
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+      localStorage.setItem("cart_totalPrice", state.totalPrice);
     },
+    //removes item from the cart
     removeCartItem: (state, action) => {
       let newItems = state.cartItems.filter(
         (item) => item.id !== action.payload.id
       );
       state.cartItems = [...newItems];
-      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+      state.totalPrice += 0; //change after data source decided
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+      localStorage.setItem("cart_totalPrice", state.totalPrice);
+    },
+    //changes quantity of an item in cart
+    changeQuantity: (state, action) => {
+      state.cartItems[action.payload.inx].quantity += action.payload.quantity;
+      state.totalPrice += 0; //change after data source decided
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+      localStorage.setItem("cart_totalPrice", state.totalPrice);
     },
   },
 });
 
-export const { addCartItem, removeCartItem } = CartSlice.actions;
+export const { addCartItem, removeCartItem, changeQuantity } =
+  CartSlice.actions;
 export default CartSlice.reducer;
---
+++ src/store/slices/wishlistSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const WishListSlice = createSlice({
+  name: "wishlist",
+  initialState: {
+    wishlistItems: localStorage.getItem("wishlist") | [],
+  },
+  reducers: {
+    addWishListItem: (state, action) => {
+      state.wishlistItems = [...state.wishlistItems, action.payload];
+      localStorage.setItem("wishlist", JSON.stringify(state.wishlistItems));
+    },
+    removeWishlistItem: (state, action) => {
+      let newItems = state.wishlistItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.wishlistItems = [...newItems];
+      localStorage.setItem("wishlist", JSON.stringify(state.wishlistItems));
+    },
+  },
+});
