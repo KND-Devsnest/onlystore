0575e07d70f3c2fe04377a1e006b52e3017697db
Swanand Buva <swanandev@gmail.com>
2021-08-17 18:03:52 +0530

    Bug Fix 1

--- src/store/slices/orderSlice.js
+++ src/store/slices/orderSlice.js
@@ -1,16 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 const OrderSlice = createSlice({
-    name : "order",
-    initialState:{
-        //localStorage.getitem('orderItems') | orderItems : [],
+  name: "order",
+  initialState: {
+    orderItems: localStorage.getitem("orderItems") | [],
+  },
+  reducers: {
+    addOrderItem: (state, action) => {
+      state.orderItems = [...state.orderItems, action.payload];
+      localStorage.setItem("orderItems", JSON.stringify(state.orderItems));
     },
-    reducers : {
-        addOrderItem: (state, action) => {
-            state.orderItems = [...state.orderItems, action.payload];
-            localStorage.setItem("orderItems", JSON.stringify(state.orderItems));
-        },
-    },
+  },
 });
 export const { addOrderItems } = OrderSlice.actions;
 export default OrderSlice.reducer;
-
