ee1be16ca76e3657835d149550fdb82aa2f64103
Ritik947 <gourritikkailash@gmail.com>
2021-08-17 17:00:36 +0530

    router implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import { AppBar, Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    fontSize: "1.5rem",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -21,15 +25,21 @@ const Navbar = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <div className={classes.menuButton}></div>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
           <Link to="/" className={classes.title}>
             OnlyStore
           </Link>
           <div style={{ display: "flex" }}>
             <Link to="/orders" style={{ marginRight: "1rem" }}>
-              ❤
+              Orders
             </Link>
-            <div>🛒</div>
           </div>
         </Toolbar>
       </AppBar>
