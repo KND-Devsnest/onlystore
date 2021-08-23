0b929ce8a93e0ea1896636ac5a8d41d2cb5b977a
seebham <shubhamisdev@gmail.com>
2021-08-23 16:57:26 +0530

    fixed minHeight

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,6 +13,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
     margin: theme.spacing(2),
+    minHeight: "70vh",
   },
   form: {
     width: "90%",
